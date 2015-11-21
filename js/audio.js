495720c6f2f74bb5a4001bcb0f19757dfe215068
Tonia B <toniabeglari@gmail.com>
2015-11-20 21:05:49 -0800

    basic audio data

---
+++ js/audio.js
@@ -0,0 +1,70 @@
+var context;
+var source, sourceJs;
+var analyser;
+var url = '../audio/song.mp3';
+var array = new Array();
+var boost = 0;
+
+try {
+	context = new AudioContext();
+}
+catch(e) {
+	alert('Web Audio API is not supported in this browser');
+}
+
+var request = new XMLHttpRequest();
+request.open("GET", url, true);
+request.responseType = "arraybuffer";
+
+request.onload = function() {
+	context.decodeAudioData(
+		request.response,
+		function(buffer) {
+			if(!buffer) {
+				alert('Error decoding file data');
+				return;
+			}
+
+			sourceJs = context.createScriptProcessor(2048, 1, 1);
+			sourceJs.buffer = buffer;
+			sourceJs.connect(context.destination);
+			analyser = context.createAnalyser();
+			analyser.smoothingTimeConstant = 0.6;
+			analyser.fftSize = 512;
+
+			source = context.createBufferSource();
+			source.buffer = buffer;
+			source.loop = true;
+
+			source.connect(analyser);
+			analyser.connect(sourceJs);
+			source.connect(context.destination);
+
+			sourceJs.onaudioprocess = function(e) {
+				array = new Uint8Array(analyser.frequencyBinCount);
+				analyser.getByteFrequencyData(array);
+				boost = 0;
+				for (var i = 0; i < array.length; i++) {
+		            boost += array[i];
+		        }
+		        boost = boost / array.length;
+			};
+
+			// popup
+			play();
+		},
+		function(error) {
+			alert('Decoding error:' + error);
+		}
+	);
+};
+
+request.onerror = function() {
+	alert('buffer: XHR error');
+};
+
+request.send();
+
+function play() {
+	source.start(0);
+}
